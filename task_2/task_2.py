5b76e00d78aecf4cd5957af185cd2aec43614e29
cookiejar1721 <120406684+cookiejar1721@users.noreply.github.com>
2024-11-06 13:27:38 +0500

    Add files via upload

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -3,6 +3,8 @@ import numpy as np
 def generate_random_data(mean, variance, num_samples):
     return np.random.randint(max(mean - variance, 0), min(mean + variance + 1, 90), num_samples)
 
+def calculate_department_average(threat_scores):
+    return np.mean(threat_scores)
 
 def calculate_aggregated_threat(department_data, department_importance):
     total_importance = sum(department_importance.values())
